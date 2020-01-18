4d3511d7ce298c2df502e4f314401c34813893d0
Vinicius Espinosa <viniespinosa.developer@gmail.com>
2020-01-17 21:32:44 -0300

    Projeto Iniciado: Testando...

---
+++ Projeto-TesteGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto_TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime t = new DateTime(2020, 1, 17, 21, 7, 56);
+
+            Console.WriteLine("Teste 1: Learning how to work with GitHub/GitBash... \n" + t.ToLongDateString());
+        }
+    }
+}
